78641ba85a28d55984f3c9578ddaa2690b71a83a
Lucas Batista <lucas.batista1santos@gmail.com>
2020-06-14 13:12:15 -0300

    Handling exceptions and clearing screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Locale;
 import java.util.Scanner;
 
+import xadrez.ExcecaoXadrez;
 import xadrez.PartidaXadrez;
 import xadrez.PecaXadrez;
 import xadrez.PosicaoXadrez;
@@ -10,21 +13,32 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(new Locale("pt", "BR"));
 		Scanner sc = new Scanner(System.in);
 		PartidaXadrez p = new PartidaXadrez();
 		
 		while (true) {
-			
-			UI.printTabuleiro(p.getPecas());
-			System.out.println();
-			System.out.print("Source: ");
-			PosicaoXadrez source = UI.lerPosicaoXadrez(sc);
-
-			System.out.println();
-			System.out.print("Target: ");
-			PosicaoXadrez target = UI.lerPosicaoXadrez(sc);
-
-			PecaXadrez capturedPiece = p.movimentoXadrez(source, target);
+			try {
+				UI.clearScreen();
+				UI.printTabuleiro(p.getPecas());
+				System.out.println();
+				System.out.print("Source: ");
+				PosicaoXadrez source = UI.lerPosicaoXadrez(sc);
+	
+				System.out.println();
+				System.out.print("Target: ");
+				PosicaoXadrez target = UI.lerPosicaoXadrez(sc);
+	
+				PecaXadrez capturedPiece = p.movimentoXadrez(source, target);
+			}
+			catch (ExcecaoXadrez e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 	}
 }
